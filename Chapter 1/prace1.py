708dd0c6a95a15c8bd1046dd4886a442169061ad
harsuuu <harshjoshi7895@gmail.com>
2022-07-01 11:53:22 +0530

    my files

---
+++ Chapter 1/prace1.py
@@ -0,0 +1,6 @@
+print('''Twinkle twinkle little star.
+How I wonder what you are.
+Up above the world so high.
+Like a diamond in the sky.
+Twinkle twinkle little star.
+How I wonder what you are.''')
