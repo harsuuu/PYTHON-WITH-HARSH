477283dcd27540f7290c7bea6d00435523d4cade
harsuuu <harshjoshi7895@gmail.com>
2022-07-07 00:10:49 +0530

    tuple in chapter 4

---
+++ Chapter 4/tuple1.py
@@ -0,0 +1,6 @@
+t = (1,4,5, 8,34)
+print(t[3])     #printing the elements of a tuple
+
+# we can not update of values of tuple
+t[3]= 5     #error
+ 
---
+++ Chapter 4/tuple2.py
@@ -0,0 +1,5 @@
+# t1 = ()        # empty tuple
+# t2 = (1)       # wrong way to declare a tuple with single element
+t2 = (1,)
+print(t2)
+# t3 = ( 1,2,3,4)   # tuple with more than one element 
