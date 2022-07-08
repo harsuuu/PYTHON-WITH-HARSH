6c110e45b6c29b3781c4ffe1ddbc7ad014ff499b
harsuuu <harshjoshi7895@gmail.com>
2022-07-08 12:21:16 +0530

    problems based on list and tupple

---
+++ Chapter 4/prob1.py
@@ -0,0 +1,10 @@
+f1= input("enter friut number 1 :")
+f2= input("enter friut number 2 :")
+f3= input("enter friut number 3 :")
+f4= input("enter friut number 4 :")
+f5= input("enter friut number 5 :")
+f6= input("enter friut number 6 :")
+f7= input("enter friut number 7 :")
+
+myFruitList= [f1,f2,f3,f3,f5,f6,f7]
+print(myFruitList)
---
+++ Chapter 4/prob2.py
@@ -0,0 +1,11 @@
+m1= int(input("enter marks student number 1 :"))
+m2= int(input("enter marks student number 2 :"))
+m3= int(input("enter marks student number 3 :"))
+m4= int(input("enter marks student number 4 :"))
+m5= int(input("enter marks student number 5 :"))
+m6= int(input("enter marks student number 6 :"))
+
+m=[m1,m2,m3,m4,m5,m6]
+m.sort()
+print(m)
+print(type(m))
