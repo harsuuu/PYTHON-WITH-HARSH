b96ccacdee91a64f6c3200b83da7a4971b660878
harsuuu <harshjoshi7895@gmail.com>
2022-07-11 00:07:04 +0530

    sets --> how to make empty sets

---
+++ Chapter 5/sets3.py
@@ -0,0 +1,11 @@
+# important: This syntax will creat empty dictionary not empty sets
+
+a ={}
+print(a)
+print(type(a))      # qki iska type dict hai then we can say that it is dictionary
+
+# now we will see how to make empty sets using below syntax
+
+b= set()
+print(b)
+print(type(b))
