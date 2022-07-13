494f51850683d70b56df53a84cd31b01aa5faf13
harsuuu <harshjoshi7895@gmail.com>
2022-07-13 16:06:50 +0530

    in and is

---
+++ Chapter 6/in.py
@@ -0,0 +1,3 @@
+a =[2,3,4,5,6,66]
+print(2 in a)           #TRUE
+print(34 in a)          #FALSE
---
+++ Chapter 6/is.py
@@ -0,0 +1,8 @@
+from pickle import NONE
+
+
+a = NONE
+if(a is NONE):
+    print("YES")
+else:
+    print("NO")
