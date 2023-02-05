38be263cc17ddfda179304989c5a7d9bdda53356
harsuuu <harshjoshi7895@gmail.com>
2023-02-05 22:00:13 +0530

    very basic

---
+++ UDEMY/FIRST/data_type.py
@@ -0,0 +1,17 @@
+#int and float
+a=1
+b=2
+c=a+b
+d=123.4
+print(c)
+print(type(c))
+print(c+2)
+print(type(c+2))
+print(c-1)
+print(c*3)
+print(c/3)
+print(type(c/3))
+print(type(d))
+print(2 ** 2)     #2 ki 2
+print(2 ** 4)
+print(5%4)        #remainder
---
+++ UDEMY/FIRST/math_function.py
@@ -0,0 +1,4 @@
+#math funcion
+print(round(3.1))
+print(round(3.9))
+print(abs(-10))    #it returns absoluate value of number
