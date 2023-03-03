e31e9d62f7bdd1f326493edbb37d656713ae720a
harsuuu <harshjoshi7895@gmail.com>
2023-03-03 23:02:14 +0530

    python

---
+++ College python/outliers/1.py
@@ -0,0 +1,5 @@
+import pandas as pd   #pd indicate pandas.
+#agr koi error aaye to run m jakr pip install pandas kr lo.
+df=pd.read_csv("heights.csv")       #df is variable name.
+#I want to see the content of df
+df
---
+++ College python/outliers/new.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df=pd.read_csv("C:\Users\harsh\Desktop\PYTHON\COLLEGE--PYTHON\outliers\heights.csv")
+df.head(10)
+#outliers--> A outkier is a data point in a data set that is distant from all other observations.
+'''impact havin outliers in a dataset ?-->1. It causes various problem during our statistical '''
